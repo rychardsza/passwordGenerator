c75780c729183e52d5fc5fcb3fb59e05e714f239
rychardsza <rychardzousa@gmail.com>
2023-08-09 18:26:40 -0300

    portfolio project1

---
+++ main.js
@@ -0,0 +1,17 @@
+let emptyString = '';
+let numbers = [0,1,2,3,4,5,6,7,8,9];
+let symbols = '!@#$%&*';
+let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const passwordOutput = document.getElementById("passwordOutput");
+const slider = document.getElementById("myRange");
+const checkBox = document.getElementsByClassName("checkBox");
+const generatePasswordBtn = document.getElementById("generatePasswordBtn");
+
+
+
+
+
+
+// while (emptyString.length < 10) {
+//     emptyString += characters[Math.floor(Math.random() * characters.length)];
+// } 
